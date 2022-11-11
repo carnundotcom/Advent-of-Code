3592cfba701d816398878bd9ffba3741d7cd6635
CarnunMP <admin@carnun.com>
2022-11-11 11:18:40 +0000

    Revamp utils with babashka.fs and auto input fetching; solve y2021/d1

--- src/utils.clj
+++ src/utils.clj
@@ -1,6 +1,13 @@
 (ns utils
-  (:require [clojure.java.io :as io]))
+  (:require [babashka.fs :as fs]
+            [clojure.string :as str]))
 
-(defn read-string-seq [filename]
- (with-open [r (io/reader (str "data/" filename ".txt"))]
-                    (doall (line-seq r))))
+(defn input-filename []
+  (as-> (str *ns*) s
+    (str/split s #"\.")
+    (interleave (repeat "/") s)
+    (apply str s)
+    (str "data" s ".txt")))
+
+(defn input []
+  (fs/read-all-lines (input-filename)))
--- src/y2021/d1.clj
+++ src/y2021/d1.clj
@@ -1,1 +1,34 @@
-(ns y2021.d1)
+(ns y2021.d1
+  (:require [hyperfiddle.rcf :refer [tests]]
+            [utils :as u]))
+
+(def dummy-input '(199 200 208 210 200 207 240 269 260 263))
+(def input (map parse-long (u/input)))
+
+; ---
+
+(defn part-1 [input]
+  (->> (partition 2 1 input)
+       (filter (fn [[a b]] (< a b)))
+       count))
+
+(tests
+  (part-1 dummy-input) := 7)
+
+(comment
+  (part-1 input) ; => 1692
+  )
+
+; ---
+
+(defn part-2 [input]
+  (->> (partition 3 1 input)
+       (map (partial reduce +))
+       part-1))
+
+(tests
+  (part-2 dummy-input) := 5)
+
+(comment
+  (part-2 input) ; => 1724
+  )
