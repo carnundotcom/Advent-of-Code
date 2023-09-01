8f1b13a360fe6be1919eee4f91cae669af28be75
CarnunMP <admin@carnun.com>
2023-09-01 21:37:05 +0100

    Tidy and document `utils/input`

--- src/utils.clj
+++ src/utils.clj
@@ -2,9 +2,17 @@
   (:require [babashka.fs :as fs]
             [clojure.string :as str]))
 
-(defn input-filename []
-  (->> (str/split (str *ns*) #"\.")
+
+;; --- input ---
+
+(defn- input-filename [ns]
+  (->> (str/split (str ns) #"\.")
        (apply format "data/%s/%s.txt")))
 
-(defn input []
-  (fs/read-all-lines (input-filename)))
+(defn input
+  "Read all lines from puzzle input into a vector of strings.
+
+  By default, reads the puzzle input corresponding to the calling namespace."
+  ([] (input *ns*))
+  ([ns]
+   (fs/read-all-lines (input-filename ns))))
