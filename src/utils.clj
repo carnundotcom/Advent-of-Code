12ab4d007b33535f6568f64293b2768977fbeb0d
CarnunMP <admin@carnun.com>
2023-09-03 15:09:42 +0100

    Golf y2015/d4

--- src/utils.clj
+++ src/utils.clj
@@ -1,6 +1,8 @@
 (ns utils
   (:require [babashka.fs :as fs]
-            [clojure.string :as str]))
+            [clojure.string :as str])
+  (:import java.security.MessageDigest
+           java.math.BigInteger))
 
 
 ;; --- input ---
@@ -24,3 +26,12 @@
   "Returns the index of the first item where (f item) is truthy, else nil."
   [f coll]
   (first (keep-indexed #(when (f %2) %1) coll)))
+
+
+;; --- hashing ---
+
+;; with thanks to https://gist.github.com/jizhang/4325757?permalink_comment_id=2196746#gistcomment-2196746
+(defn md5 [^String s]
+  (let [algorithm (MessageDigest/getInstance "MD5")
+        raw (.digest algorithm (.getBytes s))]
+    (format "%032x" (BigInteger. 1 raw))))
--- src/y2015/d4.clj
+++ src/y2015/d4.clj
@@ -1,5 +1,6 @@
 (ns y2015.d4
-  (:require [hyperfiddle.rcf :refer [tests]]
+  (:require [clojure.string :as str]
+            [hyperfiddle.rcf :refer [tests]]
             [utils :as u]))
 
 
@@ -7,38 +8,56 @@
 
 ; ---
 
-(do (defn chug-lazy
-      "Chugs through hashes until one is found that starts with the specified number of zeros."
-      [s n]
-      (let [found? #(= (subs (u/md5 (str s %)) 0 n)
-                       (apply str (repeat n \0)))]
-        (some #(when (found? %) %)
-              (range 1 ##Inf))))
-    (time (chug-lazy input 5)))
+(defn chug-lazy-1
+  "Chugs through hashes until one is found that starts with the specified number of zeros."
+  [s n]
+  (let [found? #(= (subs (u/md5 (str s %)) 0 n)
+                   (apply str (repeat n \0)))]
+    (some #(when (found? %) %)
+          (range 1 ##Inf))))
 
-(do (defn chug-imperative
-      "Chugs through hashes until one is found that starts with the specified number of zeros."
-      [s n]
-      (let [found? #(= (subs (u/md5 (str s %)) 0 n)
-                       (apply str (repeat n \0)))]
-        (loop [salt 1]
-          (if (found? salt)
-            salt
-            (recur (inc salt))))))
-    (time (chug-imperative input 5)))
+(defn chug-lazy-2 ; streamline check
+  "Chugs through hashes until one is found that starts with the specified number of zeros."
+  [s n]
+  (let [target (apply str (repeat n \0))]
+    (some #(when (str/starts-with? (u/md5 (str s %)) target) %)
+          (range 1 ##Inf))))
 
+(defn chug-imperative-1 ; loop instead of traversing a lazy seq
+  "Chugs through hashes until one is found that starts with the specified number of zeros."
+  [s n]
+  (let [target (apply str (repeat n \0))]
+    (loop [salt 1]
+      (if (str/starts-with? (u/md5 (str s salt)) target)
+        salt
+        (recur (inc salt))))))
 
 (tests
   (subs (u/md5 "abcdef609043") 0 5) := "00000"
   )
 
 (comment
-
-  (time (chug-lazy input 5))
+  (time (chug-lazy-1 input 5))
   ;;="Elapsed time: 496.611692 msecs"
   ;;282749
 
-  (time (chug-lazy input 6))
+  (time (chug-lazy-2 input 5))
+  ;;="Elapsed time: 399.399964 msecs" ; marginally faster
+  ;282749
+
+  (time (chug-lazy-1 input 6))
   ;;="Elapsed time: 17432.438005 msecs"
+  ;9962624
+
+  (time (chug-lazy-2 input 6)) ; a few seconds shaved off, nice!
+  ;;="Elapsed time: 14137.075444 msecs"
+  ;9962624
+
+  (time (chug-imperative-1 input 5)) ; but imperative...
+  ;;="Elapsed time: 385.39226 msecs"
+  ;282749
+
+  (time (chug-imperative-1 input 6)) ; ... wins here. just!
+  ;;="Elapsed time: 13379.78284 msecs"
   ;9962624
   )
